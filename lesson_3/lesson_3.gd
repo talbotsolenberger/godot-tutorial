1c442ccd32760394013453824d6257c7c24ad664
talbotsolenberger <talbotsolenberger@gmail.com>
2026-01-17 16:10:44 -0500

    Updated lesson_3.gd for clarity

--- lesson_3/lesson_3.gd
+++ lesson_3/lesson_3.gd
@@ -17,10 +17,15 @@ extends Node
 # Data can be overwritten with the `=` too. The data inside a variable will 
 # always be what it was most recently set to. Try changing the data. In the 
 # empty line in between when the variable was set and print(), try setting 
-# `message` to be something else. Then run the code again.
+# `message` to be something else. Note that you do not need to use `var`. 
+# That is only used when creating a variable. Then run the code again.
+#
+# When you have overwritten `message` correctly, proceed to the next lesson.
 # --------------------------------------------------------------------------- #
 
 func _ready():
 	var message = "This is my message."
+	# PUT CODE BETWEEN THESE LINES ------------------------------------------ #
 
+	# ----------------------------------------------------------------------- #
 	print(message)
